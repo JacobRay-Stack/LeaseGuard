dcdbd8f0fc897c9e0b7d94ce2dea041f8c3cd2d4
JacobRay-Stack <jacobray5858@gmail.com>
2026-03-06 14:48:37 -0500

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -17,7 +17,7 @@ function supabaseReq(path, method, body, token) {
     }
 
     const req = https.request(
-      { hostname: new URL(process.env.SUPABASE_URL).hostname, path, method, headers },
+      { hostname: new URL(process.env.SUPABASE_URL || 'https://gbzyzsxuxwmdlzagkrvt.supabase.co').hostname, path, method, headers },
       (res) => {
         let d = '';
         res.on('data', (c) => (d += c));
